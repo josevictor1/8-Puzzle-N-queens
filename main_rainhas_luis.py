26b464da601f64996d0e259b1f2273ad716bdbd2
josevictor1 <josevictorpatos@gmail.com>
2016-09-26 15:32:24 -0300

    heuristica funcionando

--- main_rainhas_luis.py
+++ main_rainhas_luis.py
@@ -34,6 +34,17 @@ def embaralha(tabuleiro):
 
     tabuleiro["pecas"] = rainhas
 
+def heuristica(l):
+     h = len(l)
+     soma = 0
+     for i in range(h):
+        for j in range(h):
+            if i != j and (l[i] - i == l[j] - j or l[i] + i == l[j] + j):
+                soma = soma + 1 
+                print "primeira", l[i],i
+                print "segunda", l[j],j
+     return soma
+
 
 def main():
     tabuleiro = cria_tabuleiro()
